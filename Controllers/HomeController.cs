13e6bfa2325e7ea07fcce578eea635fa45b0db63
joshua <joshuahsientan@gmail.com>
2022-11-01 13:55:13 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeAPIPracticev2.Models;
+using System.Diagnostics;
+
+namespace RecipeAPIPracticev2.Controllers
+{
+    public class HomeController : Controller
+    {
+        RecipeDAL api = new RecipeDAL();
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Results r = api.SearchRecipe("Pasta");
+            return View(r);
+        }
+
+        public IActionResult SearchByTitle()
+        {
+            return View();
+        }
+
+        public IActionResult Results(string title)
+        {
+            Results r = api.SearchRecipe(title);
+            return View(r);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RecipeDAL.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using RestSharp;
+
+namespace RecipeAPIPracticev2.Models
+{
+    public class RecipeDAL
+    {
+        public string key = "2202bc6f8655406b84a2ca9ed4af6a79";
+
+        public Results SearchRecipe(string keyword)
+        {
+            string url = $"https://api.spoonacular.com/recipes/complexSearch?query={keyword}&apiKey={key}"; //replace ?s= to ?i=, per omdbapi website
+
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest();
+            var response = client.GetAsync<Results>(request);
+            Results r = response.Result;
+            return r;
+        }
+        
+    }
+}
